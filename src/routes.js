3bf5a74a17476316c993a1e9830333c5bdd9afcb
Alfredo Henrique <alfredohenrique@alu.ufc.br>
2019-12-27 13:08:33 -0300

    models loader created

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  res.json({ message: 'Hello' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Alfredo Henrique',
+    email: 'alfredohenrique@alu.ufc.br',
+    password_hash: '28374yjdasbdkkashdj',
+  });
+
+  res.json(user);
 });
 
 export default routes;
