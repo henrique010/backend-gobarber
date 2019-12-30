4080af97a82cca5f3fee1856c45c67fe18c901e8
Alfredo Henrique <alfredohenrique@alu.ufc.br>
2019-12-30 16:15:33 -0300

    authetication middleware, created

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,14 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import Middlewares from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(Middlewares);
+routes.put('/users', UserController.update);
 
 export default routes;
