b33040dae8e4990e2aef52017df2b3167e341894
Alfredo Henrique <alfredohenrique@alu.ufc.br>
2020-02-19 10:39:49 -0300

    appoitment creation, done

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import Middlewares from './app/middlewares/auth';
 import multerConfig from './config/multer';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -20,5 +21,7 @@ routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
 
 export default routes;
