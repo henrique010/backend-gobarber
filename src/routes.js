4e635cd83640c5ac76c7b7817b88c8733b115d45
Alfredo Henrique <alfredohenrique@alu.ufc.br>
2020-02-20 13:33:01 -0300

    route to mark notification as read, added

--- src/routes.js
+++ src/routes.js
@@ -30,5 +30,6 @@ routes.get('/appointments', AppointmentController.index);
 routes.get('/schedules', ScheduleController.index);
 
 routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 export default routes;
