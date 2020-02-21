b5d1c26fca1a35686c364b44f94bbeaff21ee206
Alfredo Henrique <alfredohenrique@alu.ufc.br>
2020-02-21 13:43:31 -0300

    added sending email when appointment was canceled

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '04787cbe71bfb4',
+    pass: 'cf0858e2cc598b',
+  },
+  default: {
+    from: 'Esquipe GoBarber <noreply@gobarber.com>',
+  },
+};
